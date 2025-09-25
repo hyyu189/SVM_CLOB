5e27228bdf98ee55240952734dbbb70701ffb469
Haiyang Yu <103476745+hyyu189@users.noreply.github.com>
2025-09-24 20:09:12 -0700

    front-end vercel

--- svm_clob_frontend/vite.config.ts
+++ svm_clob_frontend/vite.config.ts
@@ -4,4 +4,23 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    global: 'globalThis',
+  },
+  resolve: {
+    alias: {
+      buffer: 'buffer',
+      crypto: 'crypto-browserify',
+      stream: 'stream-browserify',
+      util: 'util',
+    },
+  },
+  optimizeDeps: {
+    include: [
+      'buffer',
+      'crypto-browserify',
+      'stream-browserify',
+      'util',
+    ],
+  },
 })
